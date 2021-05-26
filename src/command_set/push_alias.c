6142a2f36112b7a9cd13b3e4ca3f28d550a47a76
Ledmington <68538713+Ledmington@users.noreply.github.com>
2021-05-26 23:00:41 +0200

    Now the aliases are sorted

--- src/command_set/push_alias.c
+++ src/command_set/push_alias.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "alias_utils.h"
 
@@ -6,10 +7,32 @@ void push_alias(ALIAS* new_alias){
 	/*
 		Adds the given ALIAS structure in the ALIASes stack
 	*/
-	if(new_alias == NULL){
+	if(new_alias == NULL ||
+		new_alias->str_to_sub == NULL){
 		return;
 	}
 
-	new_alias->next_alias = alias_list;
-	alias_list = new_alias;
+	// If list is empty
+	if(alias_list == NULL) {
+		new_alias->next_alias = NULL;
+		alias_list = new_alias;
+		return;
+	}
+
+	// If new_alias is "less" than alias_list
+	if(strcmp(new_alias->str_to_sub, alias_list->str_to_sub) < 0) {
+		new_alias->next_alias = alias_list;
+		alias_list = new_alias;
+		return;
+	}
+
+	// General case
+	ALIAS* tmp_alias = alias_list;
+	while(tmp_alias->next_alias != NULL &&
+		strcmp(tmp_alias->next_alias->str_to_sub, new_alias->str_to_sub) < 0) {
+		tmp_alias = tmp_alias->next_alias;
+	}
+
+	new_alias->next_alias = tmp_alias->next_alias;
+	tmp_alias->next_alias = new_alias;
 }
