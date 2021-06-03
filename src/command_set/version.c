b020c4f73e912ae33eefe2e14ce974fbf78e29fc
Ledmington <68538713+Ledmington@users.noreply.github.com>
2021-06-03 23:13:55 +0200

    Fixed spacing

--- src/command_set/version.c
+++ src/command_set/version.c
@@ -6,27 +6,27 @@
 #include "../utils.h"
 
 bool version (INPUT_STRING* input_str) {
-	/*
-		Prints the string representing the version number of the terminal
-		and the license message.
-	*/
-	const char* ver = new_string(
-		"Vector Terminal v1.0\n"
-		"Copyright (C) 2021  Ledmington\n"
-    	"This program comes with ABSOLUTELY NO WARRANTY and is free software.\n"
-    	"For details, see the file \"LICENSE\".\n");
+    /*
+        Prints the string representing the version number of the terminal
+        and the license message.
+    */
+    const char* ver = new_string(
+        "Vector Terminal v1.0\n"
+        "Copyright (C) 2021  Ledmington\n"
+        "This program comes with ABSOLUTELY NO WARRANTY and is free software.\n"
+        "For details, see the file \"LICENSE\".\n");
 
-	if (input_str->num_parameters != 0) {
-		error = too_many_parameters;
-		#if !defined(VT_TEST_MODE) || VT_TEST_MODE==0
-		print_error(help_msg);
-		#endif
-		return false;
-	}
+    if (input_str->num_parameters != 0) {
+        error = too_many_parameters;
+        #if !defined(VT_TEST_MODE) || VT_TEST_MODE==0
+        print_error(help_msg);
+        #endif
+        return false;
+    }
 
-	#if !defined(VT_TEST_MODE) || VT_TEST_MODE==0
-	appout(ver);
-	#endif
+    #if !defined(VT_TEST_MODE) || VT_TEST_MODE==0
+    appout(ver);
+    #endif
 
-	return true;
+    return true;
 }
